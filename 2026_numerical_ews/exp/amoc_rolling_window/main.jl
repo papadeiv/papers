816a0f376fcb218d4b89af8a5669163c73d317c9
papadeiv <davidepapapicco93@gmail.com>
2026-04-23 10:41:35 +1200

    Added sliding window for the AMOC timeseries

---
+++ 2026_numerical_ews/exp/amoc_rolling_window/main.jl
@@ -0,0 +1,97 @@
+"""
+    Main script
+
+Run this file to execute the simulation, analyse and plot the results.
+"""
+
+# Import the necessary packages and local modules
+include("inc.jl")
+
+# Import the simulation's scripts
+include("./scripts/proc.jl")
+include("./scripts/plot.jl")
+
+# Define the main algorithm
+function main()
+        # Import the AMOC timeseries
+        data = NCDataset("../../res/data/amoc/Atlantic_250-500m_year_0001-2200.nc")
+        time = data["time"][:]
+        latitude = data["lat"][:]
+        temperature = data["TEMP"]
+        salinity = data["SALT"]
+        N = 17
+
+        # Extract subseries until tipping
+        t = copy(time[1:idx])
+        T = copy(temperature[N,1:idx]) 
+        S = copy(salinity[N,1:idx]) 
+
+        # Define the observable (flow)
+        ϕ = T - S
+        detrended_solution = detrend(ϕ, alg = "emd", n_modes=1)
+        trend = detrended_solution.trend
+        residual = detrended_solution.residuals
+
+        ensemble = preprocess_solution(t, residual, window_size)
+        Ne = length(ensemble.trajectories)
+        Nt = length(ensemble.trajectories[1])
+
+        # Loop over the ensemble
+        ews = Vector{Float64}(undef, Ne)
+        printstyled("Computing the least-squares solutions across the ensemble\n"; bold=true, underline=true, color=:light_blue)
+        @showprogress for n in 1:Ne
+                # Generate figure and axis
+                fig = Figure(size = (1200,800))
+                ax1 = Axis(fig[1,1:2], xlabel = L"\textbf{years}", ylabel = L"\textbf{flow (250-500m)}", limits = (time[1], time[end], -27,-22))
+                ax2 = Axis(fig[2,1:2], xlabel = L"\textbf{years}", ylabel = L"\textbf{early-warning signal}", limits = (time[1], time[end], 0.0, 1.1))
+                ax3 = Axis(fig[1,3], xlabel = L"\textbf{residuals}", ylabel = L"\textbf{density}", limits = (-0.125, 0.125, nothing, 15))
+                ax4 = Axis(fig[2,3], xlabel = L"\mathbf{V(x)}", ylabel = L"\mathbf{x}", limits = (-1,1,-1,1))
+
+                # Extract windowed subseries
+                tw = copy(ensemble.timesteps[n])
+                ϕw = copy(ensemble.trajectories[n]) 
+
+                # Assemble histogram of the windowed subseries
+                σ = std(ϕw)
+                n_bins = convert(Int64, ceil(abs(maximum(ϕw)-minimum(ϕw))/(3.49*σ*(length(ϕw))^(-1.0/3.0))))
+                bins, pdf = fit_distribution(ϕw, n_bins=n_bins)
+
+                # Solve the nonlinear least-squares problem and compute the early-warning signal
+                solution = fit_potential(residual, transformation=[0.0,1.0,8.0], n_bins=n_bins).fit
+                ews[n] = analyse(solution)
+
+                # Plot and export the results
+                interval = LinRange(minimum(bins), maximum(bins), 1000)
+                domain = LinRange(-1,1,1000)
+                lines!(ax1, t, ϕ, color = (:red,0.35), linewidth = 1.0)
+                lines!(ax1, time[(idx+1):end], temperature[N,(idx+1):end] - salinity[N,(idx+1):end], color = (:black,0.35), linewidth = 1.0)
+                lines!(ax1, t[n:(Nt+n-1)], ϕ[n:(Nt+n-1)], color = :red, linewidth = 1.0)
+                poly!(ax1, Point2f[(tw[1], -27), 
+                                   (tw[end], -27),
+                                   (tw[end], -22),
+                                   (tw[1], -22),
+                                  ], color = (CtpGray, 0.1), strokecolor = :black, strokewidth = 1.0)
+                lines!(ax1, [time[idx], time[idx]], [-27, -22], color = :red, linestyle = :dash, linewidth = 2.0)
+                lines!(ax2, t[(Nt-1):(Nt+n-2)], ews[1:n], color = :red, linewidth = 4.0)
+                lines!(ax2, [time[idx], time[idx]], [-27, -22], color = :red, linestyle = :dash, linewidth = 2.0)
+                scatter!(ax2, t[(Nt+n-2)], ews[n], color = :red, markersize = 20.0, strokewidth = 1.0, strokecolor = :black)
+                barplot!(ax3, bins, pdf, color = pdf, colormap = [:white,(:red,0.5)], strokecolor = :black, strokewidth = 1.0)
+                I = (-Inf,Inf)
+                if xs(solution) > xu(solution)
+                        I = (xu(solution), +Inf) 
+                else
+                        I = (-Inf, xu(solution)) 
+                end
+                ρ(x, c) = exp(-2*V(x, c)/σ^2)
+                integral = IntegralProblem(ρ, I, solution)
+                quadrature = solve(integral, QuadGKJL(; order=20000); maxiters=10000)
+                Z = 1/(quadrature.u)
+                p(x, c) = Z*ρ(x, c)
+                lines!(ax3, interval, [p(x, solution) for x in interval], color = :red, linewidth = 4.0)
+                lines!(ax4, domain, [V(x, solution) for x in domain], color = :red, linewidth = 2.0)
+                savefig("amoc/$n.png", fig)
+        end
+end
+
+# Execute the main
+main()
